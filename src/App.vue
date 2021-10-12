b301aee8573494b1f6087fdea9787411267c79ad
Tanya Shukla <stanya97@gmail.com>
2021-10-13 00:08:38 +0530

    creating and using a store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+  <!--  $store this property point at the vuex store and state property points at the state managed by the store -->
+  <h3>{{ $store.state.counter }}</h3>
+  <button>Add 1</button>
+  </base-container>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue';
-
+import{ createStore} from 'vuex';//well create a vuex store which store our application state
 import App from './App.vue';
-
+//you can have only one store but you can have multiple state
+const store =createStore({
+ state(){ //bit like data in a component
+     return{ //this is the state of our application so thi object hold the application wide data
+ counter:0
+     };
+ }
+});
 const app = createApp(App);
-
+app.use(store);
 app.mount('#app');
